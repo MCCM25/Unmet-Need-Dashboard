555493a9d34ed60c90c4b3411e27ea9629d0ea99
Martin McCallum <mcm.gw@outlook.com>
2026-02-13 19:03:47 +0000

    Ammended cover_pafe_content script to improve layout

--- Helper Scripts/cover_page_content.R
+++ Helper Scripts/cover_page_content.R
@@ -1,32 +1,41 @@
 # Helper script to render cover page content
 
-cover_page <- function(video_id = "vid_id"){
+# define labels for table defining uptake status colours
+colour_labels <- c(
+  "Lower", "Lower Multiple", "Higher", "Higher Multiple",
+  "Normal", "Data Suppressed"
+)
+
+# convert to factor
+colour_labels <- factor(colour_labels, levels = colour_labels)
+
+# Data frame to define uptake status colours
+uptake_status_colours <- data.frame(
+  Uptake_Status = colour_labels,
+  Definition = c(
+    "Data zones where only one benefit has lower than estimated uptake.",
+    "Data zones where multiple benefits have lower than estimated uptake.",
+    "Data zones where only one benefit has higher than estimated uptake.",
+    "Data zones where multiple benefits have higher than estimated uptake.",
+    "Data zones where uptake lies within the normal range.", 
+    "Data zones where due to data suppression estimated uptake can not be calculated."
+  )
+)
+
+
+# function to render cover page content
+cover_page <- function(video_id = "vid_id", colour_data, colour_domain){
   
   tagList(
     
+    fluidRow(
+      column( width = 12,
+              
+              
+    div( class = "coverpage_layout",   
     # title
     h1("Dashboard Overview"),
     
-    p("Watch the short video below on how to use the dashboard"),
-    
-    # video embedding
-    fluidRow(
-      column(
-        8, offset = 2,
-        tags$iframe(
-          width = "100%",
-          height = "500",
-          src = paste0("https://www.youtube.com/embed/", video_id),
-          class = "guidance-video",
-          frameborder = "0",
-          allow = "accelerometer; autoplay; clipboard-write; encrypted-media;
-          gyroscope; picture-in-picture; fullscreen",
-          allowfullscreen = NA
-        )
-      )
-    ),
-    
-    br(),
     h3("What is meant by 'Unmet Need'?"),
     
     p("This dashboard aims to support users to identify local areas where there 
@@ -35,30 +44,94 @@ cover_page <- function(video_id = "vid_id"){
     
     p("The dashboard highlights data zones that may have unmet need. That is, it highlights 
     areas where the actual rates of uptake for benefits and support related to child 
-    poverty differ significantly from modelled estimated uptake of these. To model estimated uptake, 
+    poverty differ significantly from the modelled estimated uptake of these. To model estimated uptake, 
     we compare each benefit against a measure of demand, for example, Clothing Grant against the child poverty rate
     for 5 to 15 year olds, and where the child poverty rate is high, we would expect to see higher uptake of a benefit.
     The dashboard is a tool for identifying those areas where uptake differs notably from demand."),
     
-    p("Data zones coloured red are areas identified as having recorded benefit uptake notably 
-    lower than what has been estimated. Light red data zones highlight areas where only one benefit has lower uptake 
-    and dark red data zones highlight areas where multiple benefits have lower uptake."), 
+    p("The table on the right defines the colours used to represent the benefit uptake status of data zones. As well 
+      as showing where uptake is lower, the dashboard displays areas where uptake is higher than estimated as comparison of
+      these with lower uptake areas may provide insight into why ")
+  )
+
+  ),
+  
+  
+  fluidRow(
+   
+         column(
+           width = 6,
+           div(class = "cover_section",
+           h3("Data zone map colours"),
+           div(class = "table_wrapper",
+                div( class = "gt_table",
+                     colour_data %>% 
+                       gt() %>% 
+                       data_color(
+                         columns = Uptake_Status,
+                         method = "factor",
+                         palette = c("#f2a8a5", # colour Uptake Status cells
+                                     "#e02119",
+                                     "#a1f096",
+                                     "#38df20",
+                                     "white",
+                                     "grey"),
+                         domain = colour_domain) %>% 
+                       cols_label(
+                         Uptake_Status = "Uptake Status"
+                       ) %>% 
+                       tab_style( # format column names and column name cell borders
+                         style = list(cell_text(weight = "bold"), 
+                                      cell_fill(color = "#f3f3f3"),
+                                      cell_borders(sides = c("all"), color = "#003a60", weight = px(3))
+                         ),
+                         locations = cells_column_labels(columns = c(Uptake_Status, Definition)
+                         ) 
+                       ) %>% 
+                       tab_style( # format cell borders
+                         cell_borders(sides = c("all"), color = "#003a60", weight = px(3)),
+                         locations = cells_body(columns = c(Uptake_Status, Definition)
+                         )
+                       ) %>% 
+                       tab_style( # format Definition cells
+                         cell_fill(color = "#f3f3f3"),
+                         locations = cells_body(
+                           columns = Definition
+                         )
+                       ) %>% 
+                       tab_style( # format text in Uptake Status column
+                         style = cell_text(
+                           weight = "bold"
+                         ),
+                         locations = cells_body(columns = Uptake_Status)
+                       ) 
+                )
+           )),
+         
+         
+      div(class = "cover_section", 
+           # video embedding
+       h3("Watch the short video below on how to use the dashboard"),
+       br(),
+       div( class = "video_wrapper",
+        tags$iframe(
+          src = paste0("https://www.youtube.com/embed/", video_id),
+          class = "guidance-video",
+          frameborder = "0",
+          allow = "accelerometer; autoplay; clipboard-write; encrypted-media;
+          gyroscope; picture-in-picture; fullscreen",
+          allowfullscreen = NA
+        )
+       )
+       )
+      ),
+      
+      column(
+        width = 6,
     
-    p("Data zones coloured green show areas identified as having recorded benefit uptake notably higher than what has been estimated. Light green data zones highlight
-    areas where only one benefit has higher uptake and dark green data zones highlight areas where multiple 
-    benefits have higher uptake."), 
-    
-    p("Although the purpose of the dashboard is identification of areas where households may
-    not be claiming the benefits they are entitled to, the dashboard displays data for areas with higher uptake as 
-    comparison of these higher uptake areas with lower uptake areas may help to inform why uptake is lower in some areas."),
-    
-    p("Data zones coloured white are those areas where uptake lies within the normal range."),
-    
-    br(), br(),
-    
-    # tab navigation
-    fluidRow(
-      cover_section(
+      div( class = "coverpage_text",
+           
+        cover_section(
         title = "1. Data Zone Summary",
         description = "Use the map to explore data zones of interest. You can view uptake for
         all benefits or change selections to look at different combinations or view single benefits. 
@@ -95,10 +168,13 @@ cover_page <- function(video_id = "vid_id"){
         button_id = "goto_methods",
         open_tab = "Go to Methodology"
         
+        )
       )
-    )
-    
+       )
+      )
   )
+  )
+    
   
 }
 
@@ -106,14 +182,12 @@ cover_page <- function(video_id = "vid_id"){
 
 # reusable helper for action buttons
 cover_section <- function(title, description, button_id, open_tab){
-  
- column(
-   width = 6,
    div(
-     class = "cover-section",
+     class = "cover_section",
      h3(title),
      p(description),
      actionButton(button_id, open_tab, class = "btn-primary")
    )
- ) 
 }
+
+
